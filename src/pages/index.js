91cde13416cbeaecb74da29c4a38293abeb75384
tkjone <thomas.mattacchione@kirasystems.com>
2019-02-11 13:31:32 -0500

    Init blog index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,73 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import ArticleIndexItem from '../components/article-index-item'
+import Footer from '../components/Footer'
+import Header from '../components/header'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`clojurescript`, `clojure`, `javascript`]} />
-    <footer>
-      Here lives footer
-    </footer>
-  </Layout>
+const social_media_links = [
+  { title: 'Github', url: 'https://github.com/tkjone/' },
+  { title: 'Linkedin', url: 'https://www.linkedin.com/in/tmattacchione/' },
+  { title: 'Twitter', url: 'https://twitter.com/tmkjone' },
+]
+
+const EmptyIndexScreenMsg = ({ msg }) => (
+  <p className="no-articles-msg">{msg}</p>
 )
+
+const Articles = ({ articles }) => {
+  return articles.map(article => {
+    return (
+      <ArticleIndexItem
+        key={article.node.id}
+        title={article.node.frontmatter.title}
+        date={article.node.frontmatter.date}
+        description={article.node.frontmatter.summary}
+        url={article.node.fields.slug}
+      />
+    )
+  })
+}
+
+const IndexPage = ({ data }) => {
+  const articles = data.allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`clojurescript`, `clojure`, `javascript`]} />
+      <Header siteTitle="Thomas Town" />
+      {articles && articles.length ? (
+        <Articles articles={articles} />
+      ) : (
+        <EmptyIndexScreenMsg msg="No articles yet" />
+      )}
+      <Footer links={social_media_links} />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+            summary
+          }
+          fields {
+            slug
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
