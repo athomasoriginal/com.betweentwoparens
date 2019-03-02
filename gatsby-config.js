e599565d81b865d1c40ec14e5e81a1d1811b3ea2
tkjone <thomasmattacchione@gmail.com>
2019-03-02 09:31:02 -0500

    Add syntax highlighting styles to blog-posts
    And now all code shall be beautiful.

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,19 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'gatsby-code-',
+            },
+          },
+        ],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
