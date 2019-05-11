3a60882546ceb9afc18f5a4d3421de5326e44fa8
tkjone <thomasmattacchione@gmail.com>
2019-05-11 16:46:42 -0400

    Cleanup the blog index header styles
    Deleting unused styles

--- src/components/author-avatar.js
+++ src/components/author-avatar.js
@@ -2,23 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './author-avatar.css'
 
-const DefaultAuthorPicture = () => (
-  <div className="circle1">
-    <div className="circle2">
-      <div className="circle3" />
-    </div>
-  </div>
-)
-
 const AuthorAvatar = ({ name, interests, picture }) => (
   <div className="root">
-    {picture ? picture : <DefaultAuthorPicture />}
-    <h2 className="h__base h__1">{name}</h2>
-    <ul className={`ul skills`}>
-      {interests.map(interest => (
-        <li key={interest}>{interest}</li>
-      ))}
-    </ul>
+    <h1 className="site-title">Between Two Parens</h1>
+
+    <h2 className="h__base h__1 site-description">{`A blog about ${name} between two parens`}</h2>
   </div>
 )
 
