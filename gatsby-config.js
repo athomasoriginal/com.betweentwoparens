c2e0f685fa2202dd8ff7d9fbee2a223501881373
tkjone <thomasmattacchione@gmail.com>
2019-05-17 18:59:56 -0400

    Add markdown image support
    With this package we can properly render speedy image assets in our
    markdown.

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     author: `Thomas Mattacchione`,
   },
   plugins: [
+    `gatsby-plugin-sharp`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -19,19 +20,6 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     {
       resolve: 'gatsby-transformer-remark',
       options: {
@@ -40,6 +28,12 @@ module.exports = {
             resolve: `gatsby-remark-prismjs`,
             options: {
               classPrefix: 'gatsby-code-',
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
             },
           },
         ],
