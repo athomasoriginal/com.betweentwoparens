0e46ca49c3c0e306661be469a7fa07959c486b4e
tkjone <thomasmattacchione@gmail.com>
2019-05-18 10:07:38 -0400

    Replace hardocded description in the header

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './header.css'
 
-const Header = ({ title, name, interests, picture }) => (
+const Header = ({ title, description, name, interests, picture }) => (
   <div className="root">
     <h1 className="site-title">{title}</h1>
 
-    <h2 className="h__base h__1 site-description">{`A blog about ${name} between two parens`}</h2>
+    <h2 className="h__base h__1 site-description">{description}</h2>
   </div>
 )
 
