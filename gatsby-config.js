bdf21c926da5844a148108ced35aae2357b11923
tkjone <thomasmattacchione@gmail.com>
2019-05-20 10:13:13 -0400

    Remove gatsby images config
    Not currently using this

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,13 +23,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `src`,
         path: `${__dirname}/src/`,
       },
