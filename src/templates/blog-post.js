15fd727344d7ea6b3984e0c67ddabafccf4475ec
tkjone <thomasmattacchione@gmail.com>
2019-06-02 08:56:30 -0400

    Add left-arrow-icon
    Adding this icon to be placed beside the blog post "between two parens"
    title because from a design perspective I felt it could be clearer that
    there is somewhere to go back to with an icon.  Whether or not this is
    what I keep is up for future Thomas, but for now this feels like a quick
    win kind of solution

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -5,6 +5,26 @@ import { graphql, Link } from 'gatsby'
 
 import './blog-post.css'
 
+function LeftArrowIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-arrow-left"
+    >
+      <line x1="19" y1="12" x2="5" y2="12" />
+      <polyline points="12 19 5 12 12 5" />
+    </svg>
+  )
+}
+
 export default ({ data }) => {
   const siteMetadata = data.site.siteMetadata
   const post = data.markdownRemark
