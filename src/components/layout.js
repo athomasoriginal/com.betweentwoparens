18ee1361cb8291ce89bf30b13e58a0d78f9a006f
athomasoriginal <thomasmattacchione@gmail.com>
2020-03-07 10:15:22 -0500

    Subscribe - Allow custom class to layout
    ISSUE #79 There are times when we don't want the layout component to
    have the app styles, so instead we will allow ourselves to just pass a
    custom class to opt out.

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,22 +4,26 @@ import { StaticQuery, graphql } from 'gatsby'
 
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const Layout = ({ children, className }) => {
+  const classes = className ? className : "app";
+
+  return (
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => {
-      return (<main className="app">{children}</main>)
-    }}
-  />
-)
+      `}
+      render={data => {
+        return <main className={classes}>{children}</main>
+      }}
+    />
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
