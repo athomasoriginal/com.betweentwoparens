2c5fb22ee33f62e70afb94ff5486068e1d9b641e
athomasoriginal <thomasmattacchione@gmail.com>
2020-04-02 20:10:20 -0400

    Disable respectDnt GA Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -114,7 +114,7 @@ module.exports = {
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true,
+        // respectDNT: true,
       },
     },
     `gatsby-plugin-react-helmet`,
