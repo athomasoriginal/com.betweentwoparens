cc88c55a80c35ff1631fc2a005ae6598bf3dc1b3
athomasoriginal <thomasmattacchione@gmail.com>
2020-04-02 20:13:31 -0400

    Remove anon GA Analytics
    Debugging GA inactivity

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,7 +112,7 @@ module.exports = {
         // Puts tracking script in the head instead of the body
         head: true,
         // Setting this parameter is optional
-        anonymize: true,
+        // anonymize: true,
         // Setting this parameter is also optional
         // respectDNT: true,
       },
