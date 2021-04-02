64294c8c4fc83d6959347c8d231c59a8ff43de9f
athomasoriginal <thomasmattacchione@gmail.com>
2021-04-02 10:42:35 -0400

    Quite eslint - pascalcase
    [ISSUE #150]

--- src/templates/blog-content.js
+++ src/templates/blog-content.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/layout'
 import Footer from '../components/footer'
 import Header from '../components/header'
-import SEO from '../components/seo'
+import Seo from '../components/seo'
 import { graphql } from 'gatsby'
 import './prism-syntax-highlighting.css'
 
@@ -66,7 +66,7 @@ export default ({ data }) => {
         />
       }
     >
-      <SEO
+      <Seo
         title={frontmatter.title}
         description={frontmatter.summary}
         author={frontmatter.author}
